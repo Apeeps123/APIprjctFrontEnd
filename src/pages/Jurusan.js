9a5682c993eff99cc721356942a0d7d558bbb613
Apeeps123 <afifversa123@gmail.com>
2023-10-17 20:12:13 +0700

    store data

---
+++ src/pages/Jurusan.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col ,Button,Modal } from "react-bootstrap";
+import { useState,useEffect } from "react";
+import axios from 'axios';
+function Jurusan() {
+    return (
+        <Container>
+            <Row>
+                <Col> <h2>Data Jurusan</h2> </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Jurusan;
---
+++ src/template/Routing.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Mahasiswa from '../pages/Mahasiswa';
+import Jurusan from '../pages/Jurusan';
+
+function Routing() {
+    return (
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Frontend</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" to="/mhs">Mahasiswa</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/jrs">Jurusan</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <Routes>
+                    <Route path="/mhs" element={<Mahasiswa />} />
+                    <Route path="/jrs" element={<Jurusan />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default Routing;
